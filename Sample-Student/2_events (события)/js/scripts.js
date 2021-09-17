b734ff0358f0fa24bf8f930cf00960f4701ebb61
ufxzche <avidban707@gmail.com>
2021-09-17 18:25:02 +0500

    JS

---
+++ Sample-Student/2_events (события)/js/scripts.js
@@ -0,0 +1,26 @@
+window.onload = function(){
+    let text = document.querySelector('.text');
+
+    // text.onclick = function(){
+    //     text.style.background = "red";
+    // }
+
+    // text.onclick = function(){
+    //     text.style.color = "green";
+    // }
+
+    text.addEventListener('click', bgColor);
+    text.addEventListener('click', color);
+
+
+    function bgColor(){
+        text.style.background = 'red';
+    }
+
+    function color(){
+        text.style.color = 'blue';
+    }
+
+
+    console.log(text);
+}
