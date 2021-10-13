6dfe2137d661ad07233a6bd8e9e5050960d24386
ufxzche <avidban707@gmail.com>
2021-10-13 16:10:17 +0500

    JS

---
+++ Jquery_Student/JQUERY/7_Выпадающее_меню/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.hide').click(function(){
+        $('section').css({'display': 'none'});
+    })
+    $('.show').click(function(){
+        $('section').css({'display': 'block'});
+    })
+});
